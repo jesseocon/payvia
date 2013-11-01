17b6cd3fb8ade56725f51748439481d9b5203415
Jesse Ocon <jesseocon@gmail.com>
2013-11-01 14:13:42 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Payvia::Application.config.secret_token = 'f4439fd88051aa68c3bde5d4a68d36176e11a82f0fc762c217e7bdeaf4e76e16713d8e5c829498539ce5a4066a52135cef1d3d85634a872cb3cc371efa1cfc97'
